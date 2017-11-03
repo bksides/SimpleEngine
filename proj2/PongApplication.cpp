1f3b56989a99f231b51c13c4f07c5796eef99d54
William Meeks <rexmeeks@gmail.com>
2017-11-03 14:57:02 -0500

    Changed ticks in multiplayer

--- proj2/PongApplication.cpp
+++ proj2/PongApplication.cpp
@@ -71,7 +71,7 @@ bool playBoing(btManifoldPoint& cp, void* body0, void* body1)
     {
         if((body0 == ball->getRigidBody() && body1 == netPaddle->getRigidBody()) ||
         (body1 == ball->getRigidBody() && body0 == netPaddle->getRigidBody())&&
-        (firstCol || (clock() - hitClock > 2000)))
+        (firstCol || (clock() - hitClock > 10000000)))
         {
             if(firstCol)
                 firstCol = false;
