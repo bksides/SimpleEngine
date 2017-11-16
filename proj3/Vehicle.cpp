ca193ef5c64e0f022e8c5f1276550a290789a4d3
Brandon Sides <bksides@cs.utexas.edu>
2017-11-16 17:05:42 -0600

    Tried to fix vehicle.  Failed.

--- proj3/Vehicle.cpp
+++ proj3/Vehicle.cpp
@@ -2,13 +2,18 @@
 
 Vehicle::Vehicle(Ogre::SceneManager* mSceneManager)
 {
-	mesh = mSceneManager->createEntity("avalon_chair.mesh");
+	mesh = mSceneManager->createEntity(Ogre::SceneManager::PT_SPHERE);
 	mesh->setCastShadows(true);
+    mesh->setMaterialName("Ball/Skin");
 
-	btCollisionShape* groundShape = new btBoxShape(btVector3(50, 0.1, 50));
+	btCollisionShape* chairShape = new btBoxShape(btVector3(50, 0.1, 50));
 
-    btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 0, 0)));
-    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-    rigidBody = new btRigidBody(groundRigidBodyCI);
+    btDefaultMotionState* chairMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 10, 0)));
+    btScalar mass = 1;
+    btVector3 chairInertia(0, 0, 0);
+    chairShape->calculateLocalInertia(mass, chairInertia);
+    btRigidBody::btRigidBodyConstructionInfo chairRigidBodyCI(mass, chairMotionState, chairShape, chairInertia);
+    rigidBody = new btRigidBody(chairRigidBodyCI);
+    rigidBody->setCollisionFlags(rigidBody->getCollisionFlags() | btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK);
     rigidBody->setRestitution(1.0);
 }
